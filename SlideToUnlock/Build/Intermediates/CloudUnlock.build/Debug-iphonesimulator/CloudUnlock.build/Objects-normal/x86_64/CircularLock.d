c8c412f354d69772691d1a5b5ca66fb6fb2a4ae3
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-06-16 04:48:12 -0500

    switching project name to CloudUnlock;

---
+++ SlideToUnlock/Build/Intermediates/CloudUnlock.build/Debug-iphonesimulator/CloudUnlock.build/Objects-normal/x86_64/CircularLock.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/Libs/CircularLock.m \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/Libs/CircularLock.h
---
+++ SlideToUnlock/Build/Intermediates/CloudUnlock.build/Debug-iphonesimulator/CloudUnlock.build/Objects-normal/x86_64/FirstViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/FirstViewController.m \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/FirstViewController.h
