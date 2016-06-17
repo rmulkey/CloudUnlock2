b1ea7f731d4fe2194f144c157fdf1c9ef1c1153e
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-06-17 02:34:30 -0500

    Adding the History TabBar section;

---
+++ SlideToUnlock/Build/Intermediates/CloudUnlock.build/Debug-iphonesimulator/CloudUnlock.build/Objects-normal/x86_64/PermissionsViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/PermissionsViewController.m \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/PermissionsViewController.h
---
+++ SlideToUnlock/Build/Intermediates/CloudUnlock.build/Debug-iphonesimulator/CloudUnlock.build/Objects-normal/x86_64/UnlockViewController.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/UnlockViewController.m \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/UnlockViewController.h \
+  /Users/rodrigo.s.mulkey/Desktop/Dev/SlideToUnlock/SlideToUnlock/SlideToUnlock/Libs/CircularLock.h
